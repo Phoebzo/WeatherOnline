f8d71227e0754138da2ee81cb46944bb6b71ee8f
OliverLjung <64260361+OliverLjung@users.noreply.github.com>
2020-05-08 17:52:18 +0200

    update
    * updated gui (still not complete)
    * added ideas for scripts

--- script.py
+++ script.py
@@ -1,10 +1,37 @@
-# import pygame
-# import gui
-# import time
 
-# time.sleep(2)
-# interface = gui.interface()
 
-# interface.run()
+import pandas as pd
+import numpy as np
+import os
 
-print("hej")
+# For the gui
+import pygame
+import gui
+import time
+
+def main():
+    csv_frame = pd.DataFrame()
+    directory = os.fsencode(directory_in_str)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".asm") or filename.endswith(".py"): 
+            # print(os.path.join(directory, filename))
+            continue
+        else:
+            continue
+    csv_frame = load_csv_file(file_name, csv_frame)
+    time.sleep(2)
+    interface = gui.Interface()
+    interface.run()
+
+
+def load_csv_file(file_name, csv_frame):
+    try:
+        csv_frame = pd.read_csv(file_name, dtype= "category", sep= ",")
+    except FileNotFoundError:
+        print(f"\nError when reading file: File '{file_name}' does not exist")
+    return csv_frame
+
+
+if __name__ == "__main__":
+    main()
